e6e54790df1aa7d90e0b0c397dcefb551684ba25
zzinue <zzinue@gmail.com>
2024-05-07 10:09:28 -0600

    usando la herencia de titulo a pelicula y serie

--- src/com/aluracursos/screenmatch/modelos/Principal.java
+++ src/com/aluracursos/screenmatch/modelos/Principal.java
@@ -14,17 +14,6 @@ public class Principal {
         miPelicula.evalua(10);
         miPelicula.evalua(10);
         System.out.println("Media de las evaluaciones de la pelicula: "+ miPelicula.calculaMedia());
-
-
-
-
-//        com.aluracursos.screenmatch.modelos.Principal.Pelicula otraPelicula=new com.aluracursos.screenmatch.modelos.Principal.Pelicula();
-//        otraPelicula.nombre="Matrix";
-//        otraPelicula.fechaDeLanzamiento=1998;
-//        otraPelicula.duracionEnMinutos=180;
-//
-//      otraPelicula.muestraFichaTecnica();
-
     }
 
 
